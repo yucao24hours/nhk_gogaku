bf6becc6885ea729dd754de5c9969cca7270f0fc
Yuka Kato <satoimo.to.imo@gmail.com>
2016-09-20 22:41:24 +0900

    Click 出席簿 link of specified lesson

--- shusseki.rb
+++ shusseki.rb
@@ -13,4 +13,11 @@ agent.get('https://www2.nhk.or.jp/gogaku/') do |page|
     f.field_with(id: 'login_pass').value = ENV['PASSWORD']
     f.action = '/pidh02/login.do'
   end.submit
+
+  my_gogaku = my_page.link_with(text: 'マイ語学トップ').click
+
+  # TODO: 講座名はコマンドの引数で受け取れるようにする
+  href = my_gogaku.search("//div[@class='topProgramBox' and ./h2[contains(text(), '英会話タイムトライアル')]]//a[text()='出席簿']/@href")
+
+  my_gogaku.link_with(href: href.text).click
 end
